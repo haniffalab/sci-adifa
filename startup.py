1c9ed72f004d3517f3bbf743e447ffefd7169882
Dave Horsfall <dave@horsfall.dev>
2022-01-17 10:12:02 +0000

    gunicorn deployment

--- startup.py
+++ startup.py
@@ -1,6 +1,6 @@
 """ 
-In this sample, the Flask app object is contained within the hello_app *module*;
-that is, hello_app contains an __init__.py along with relative imports. Because
+The Flask app object is contained within the adifa *module*;
+that is, adifa contains an __init__.py along with relative imports. Because
 of this structure, a file like webapp.py cannot be run directly as the startup
 file through Gunicorn; the result is "Attempted relative import in non-package".
 The solution is to provide a simple alternate startup file, like this present
