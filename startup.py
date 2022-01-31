736e1187e9ddcaabde5c7f84200879d20c7da8a9
Dave Horsfall <dave@horsfall.dev>
2022-01-31 14:28:10 +0000

    update comments on need for startup.py

--- startup.py
+++ startup.py
@@ -1,11 +1,11 @@
 """ 
-The Flask app object is contained within the adifa *module*;
-that is, adifa contains an __init__.py along with relative imports. Because
-of this structure, a file like webapp.py cannot be run directly as the startup
-file through Gunicorn; the result is "Attempted relative import in non-package".
-The solution is to provide a simple alternate startup file, like this present
-startup.py, that just imports the app object. You can then just specify
-startup:app in the Gunicorn command.
+The Flask app object is contained within the adifa *module*. That is,
+adifa contains an __init__.py along with relative imports. Because of this
+structure, a file like webapp.py cannot be run directly as the startup file
+through Gunicorn; the result is "Attempted relative import in non-package".
+The solution is to provide a simple alternate startup file, like this
+present startup.py, that just imports the app object. You can then just
+specify startup:app in the Gunicorn command.
 """
 from adifa import create_app
 
