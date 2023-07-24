a323755d6d724c82d53f08db684c09461a99051d
dannda <dann.3004@gmail.com>
2023-07-24 12:14:11 +0100

    add reading disease.csv from DATA_PATH
    change disease column from csv to 'Disease'

--- adifa/utils/adata_utils.py
+++ adifa/utils/adata_utils.py
@@ -309,4 +309,10 @@ def series_median(s):
 
 
 def disease_filename():
-    return os.path.join(current_app.root_path, "data", "disease.csv")
+    return (
+        os.path.join(current_app.config.get("DATA_PATH"), "disease.csv")
+        if os.path.isfile(
+            os.path.join(current_app.config.get("DATA_PATH"), "disease.csv")
+        )
+        else os.path.join(current_app.root_path, "data", "disease.csv")
+    )
